8f73f82ebdbb93205d5d9c70f2534d759f55776c
adarshgurjar <adarshgurjar744@gmail.com>
2024-06-01 13:31:06 +0530

    data details

---
+++ data.js
@@ -0,0 +1,7 @@
+const data=[{name:'adarsh gurjar',email:'adarshgurjar744@gmail.com'},
+    {name:'parth',email:'parthbhate@gmail.com'},
+    {
+        name:'palkesh',email:'palkeshsharma@gmail.com'
+    }
+]
+module.exports=data;
